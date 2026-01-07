38d89780d62826f2b474c93fe55e8e29cbd27a74
Oleksii Stepanyk <o.stepanik06@gmail.com>
2026-01-07 12:36:01 +0200

    code format

--- GlobalConfig.h
+++ GlobalConfig.h
@@ -4,11 +4,13 @@
 #include <string>
 
 namespace GlobalConfig {
-constexpr int WINDOW_WIDTH = 1280;
-constexpr int WINDOW_HEIGHT = 720;
-constexpr int GRID_COLUMNS = 4;
-constexpr double GPA_DEFAULT_VALUE = 0.0;
-std::string GPA_STRING = "GPA: ";
+    constexpr int WINDOW_WIDTH = 1280;
+    constexpr int WINDOW_HEIGHT = 720;
+
+    constexpr int GRID_COLUMNS = 4;
+
+    constexpr double GPA_DEFAULT_VALUE = 0.0;
+    std::string GPA_STRING = "GPA: ";
 }
 
 #endif // GLOBALCONFIG_H
